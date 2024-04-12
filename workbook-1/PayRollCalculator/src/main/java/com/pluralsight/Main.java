84b52a2b2a0e9f07894f4783bb55998e055990d1
Nassah <tt@tts-MacBook-Pro.local>
2024-04-12 15:35:47 -0700

    Excercise PayRollCalculator

--- workbook-1/PayRollCalculator/src/main/java/com/pluralsight/Main.java
+++ workbook-1/PayRollCalculator/src/main/java/com/pluralsight/Main.java
@@ -15,18 +15,18 @@ public class Main {
 
 
     // prompt user to enter hours worked (floating number)
-    System.out.println("Hello " + employeeName + " how many hours have you worked?");
+    System.out.printf("Hello %s how many hours have you worked?\n", employeeName);
     //System.out.print("Hello %s how many hours have you worked?", employeeName);
     hoursWorked = Input.nextDouble();
 
     //prompt user to enter their pay rate (floating number)
-    System.out.println("What's your pay rate?");
+    System.out.print("What's your pay rate?\n");
     payRate = Input.nextDouble();
 
 
     //Calculate gross pay
     gross = hoursWorked * payRate;
-    System.out.println("Your gross pay is " + gross);
+    System.out.printf("Your gross pay is %.2f ", gross);
 
 
     }
